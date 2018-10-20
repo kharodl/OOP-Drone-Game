d84a7ee71d1d73ea30efd3ffe3206c3f5c3220e8
Lovejit <kharodl@gmail.com>
2018-10-20 13:47:32 -0700

    Scores.java initial

--- src/Scores.java
+++ src/Scores.java
@@ -1,3 +1,21 @@
-public class Scores
+import javax.swing.*;
+
+public class Scores extends JLabel
 {
+	private int total, won;
+
+	public Scores()
+	{
+		super(0 + " / " + 0, CENTER);
+		total = won = 0;
+		//super.setAlignmentY(TOP);
+	}
+
+	public void gameEnded(boolean win)
+	{
+		if (win)
+			won++;
+		total++;
+		super.setText(won + " / " + total);
+	}
 }
