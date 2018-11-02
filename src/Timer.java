dc04fd7a9b6dcedf7238e5adaf95ea9754ba48c5
Adham Kamel <42755766+adham-kamel@users.noreply.github.com>
2018-11-01 18:34:51 -0700

    Update Timer.java

--- src/Timer.java
+++ src/Timer.java
@@ -2,7 +2,7 @@
  * Timer.java
  *
  * @author Sebrianne Ferguson
- * Last Edited: 10/20/2018
+ * Last Edited: 11/1/2018
  * Purpose: To change the position of the airplanes increment the x positions for the
  * airplanes every certain milliseconds.
  */
@@ -18,16 +18,16 @@ public class Timer {
 	 *               if it's false, then the methods will recheck the condition and stop the movement
 	 *               this would be useful in the case where the stopwatch has timed out.
 	 */
-	public void airplaneTimer(Airplane p, boolean resume) {
+	public void airplaneTimer(PlaneShape p, boolean resume) {
 		time = System.currentTimeMillis();
 		while (resume) {
 			currentTime = System.currentTimeMillis();
 			if (((currentTime - time) / 1000) % 60 == 0) { //a whole second has gone by
 				if (p.getX() > 200) { //arbitrary number for the end of the screen, can change later
-					p = new Airplane(0, p.getY(), p.getIconWidth()); //will draw a new plane
+					p = new PlaneShape(0, p.getY(), p.getIconWidth()); //will draw a new plane
 				}
 				else { //in the middle of the screen
-					p = new Airplane(p.getX() + 10, p.getY(), p.getIconWidth()); //will draw a new plane
+					p = new PlaneShape(p.getX() + 10, p.getY(), p.getIconWidth()); //will draw a new plane
 				}
 			}
 		}
